2b72ed20eb94cb9c2670c3f39945b9b958b7a6bf
redhubget <ragureddy7412@gmail.com>
2025-08-29 12:55:34 +0530

    Create Splash.jsx

---
+++ src/components/Splash.jsx
@@ -0,0 +1,10 @@
+export default function Splash() {
+  return (
+    <div className="splash-wrap">
+      <div>
+        <img src="/govv-logo.png" alt="GoVV" className="splash-logo" />
+        <div className="splash-brand">Go VV</div>
+      </div>
+    </div>
+  );
+}
